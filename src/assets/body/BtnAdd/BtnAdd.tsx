85aa4904d90fffe4bdb18f48c25ff7f3e8aa3100
mostafa <mostafa.h.web@gmail.com>
2023-09-03 10:40:06 +0330

    update array in object in react/ Practice

--- src/assets/body/BtnAdd/BtnAdd.tsx
+++ src/assets/body/BtnAdd/BtnAdd.tsx
@@ -15,6 +15,11 @@ export default function BtnAdd() {
     
     const [arr, setarr] = useState(['salam','aleik'])
     
+    const [fixBug, setFixBug] = useState([
+        {name: 'fix header' , id: 1, fix: false},
+        {name: 'fix footer' , id: 2, fix: false}
+    ])
+    
     function add() {
         setShoes({...shoes, price: 50, prop:{ ...shoes.prop, color: 'yellow'}, brand: 'adidas'})
         
@@ -24,8 +29,12 @@ export default function BtnAdd() {
         setarr(arr.filter(tag => tag !== 'salam'))
         // Update
         setarr(arr.map(tag => tag === 'aleik' ? 'khodafez' : tag))
+        
+        //update Array in Obejct
+        setFixBug(fixBug.map(item => item.id === 1 ? {...item, fix: true} : item))
     }
     
+
     return(
         <div>
             {shoes.price}
