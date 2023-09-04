6224dae993b1d03767f00642e1c2bc1e6601da77
mostafa <mostafa.h.web@gmail.com>
2023-09-05 00:26:47 +0300

    complete 1 step at project/form-validation

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Alert from "./assets/body/Alert/alert.tsx";
 // import {Fragment} from "react";
 import Button from "./assets/body/Alert/Button.tsx";
-import {useState} from "react";
+import { useState } from "react";
 // import React from "react";
 import RightSide from "./assets/body/RightSide/RightSide.tsx";
 import LeftSide from "./assets/body/LeftSide/LeftSide.tsx";
@@ -13,77 +13,119 @@ import Navbar from "./assets/body/ProductUser/Navbar/Navbar.tsx";
 import ProductItem from "./assets/body/ProductUser/ProductItem/ProductItem.tsx";
 import BtnClear from "./assets/body/ProductUser/BtnClear/BtnClear.tsx";
 import TextArea from "./assets/body/TextArea/TextArea.tsx";
-import Form from './assets/body/Form/Form.tsx'
-import Table from './assets/body/ExpenceTracker/Table.tsx'
+import Form from "./assets/body/Form/Form.tsx";
+import Table from "./assets/body/ExpenceTracker/Table.tsx";
+
 function App() {
-    
-    function changeAlert() {
-        setShowAlert(true)
-    }
-    
-    function closeAlert() {
-        setShowAlert(false)
-    }
-    
+  function changeAlert() {
+    setShowAlert(true);
+  }
 
-    const [showAlert, setShowAlert] = useState(false)
-    
-    
-    
-    const [itemShop, setItemShop] = useState(['item 1', 'item 2', 'item 3'])
-    
-    
-    const userArray = [
-        {id: 1, Description: 'aaa', Amount: 12, Category: 'Geroceries' },
-        {id: 1, Description: 'aaa', Amount: 12, Category: 'Geroceries' },
-        {id: 1, Description: 'aaa', Amount: 12, Category: 'Geroceries' },
-        {id: 1, Description: 'aaa', Amount: 12, Category: 'Geroceries' }
-    ]
-    
-    return (
-        <>
-        
-        {showAlert && <Alert close={closeAlert}>salamm were <span className={'bg-warning'}>children</span></Alert>}
-            
-            <br/>
-            <Button color={'primary'} clickF={changeAlert}>show</Button>
-            <br/>
-            <br/>
-            <br/>
-            <RightSide></RightSide>
-            <LeftSide></LeftSide>
-            <br/>
-            <MiddleSide></MiddleSide>
-            <br/><br/><br/>
-            <SwitchLike onclickP={() => console.log('clicked')}></SwitchLike>
-            <br/>
-            <BtnAdd></BtnAdd>
-            <br/><br/>
-            
-            <Navbar coutItems={itemShop.length}></Navbar>
-            <ProductItem children={itemShop.map(item => <li>{item}</li>)}></ProductItem>
-            <BtnClear clearF={() => setItemShop([])}></BtnClear>
+  function closeAlert() {
+    setShowAlert(false);
+  }
 
-            <br/><br/><br/>
-            
-            <TextArea maxChar={10} key={'textvalue'}>lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumloremipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumloremipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumloremipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumloremipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumloremipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumloremipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</TextArea>
+  const [showAlert, setShowAlert] = useState(false);
 
-            
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            
-            <Form></Form>
-            
-            <br/><br/><br/>
-            
-            <Table expense={}></Table>
-            
-            
-            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-            
-        </>
-    );
+  const [itemShop, setItemShop] = useState(["item 1", "item 2", "item 3"]);
+
+  const [SelectedItem, setSelectedItem] = useState([
+    { id: 1, Description: "aaa", Amount: 12, category: "Geroceries" },
+    { id: 2, Description: "bbb", Amount: 12, category: "Geroceries" },
+    { id: 3, Description: "ccc", Amount: 12.5, category: "Geroceries" },
+    { id: 4, Description: "ddd", Amount: 12, category: "Geroceries" }])
+
+    
+  return (
+    <>
+      {showAlert && (
+        <Alert close={closeAlert}>
+          salamm were <span className={"bg-warning"}>children</span>
+        </Alert>
+      )}
+
+      <br />
+      <Button color={"primary"} clickF={changeAlert}>
+        show
+      </Button>
+      <br />
+      <br />
+      <br />
+      <RightSide></RightSide>
+      <LeftSide></LeftSide>
+      <br />
+      <MiddleSide></MiddleSide>
+      <br />
+      <br />
+      <br />
+      <SwitchLike onclickP={() => console.log("clicked")}></SwitchLike>
+      <br />
+      <BtnAdd></BtnAdd>
+      <br />
+      <br />
+
+      <Navbar coutItems={itemShop.length}></Navbar>
+      <ProductItem
+        children={itemShop.map((item) => (
+          <li>{item}</li>
+        ))}
+      ></ProductItem>
+      <BtnClear clearF={() => setItemShop([])}></BtnClear>
+
+      <br />
+      <br />
+      <br />
+
+      <TextArea maxChar={10} key={"textvalue"}>
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
+        ipsumlorem ipsumlorem ipsumlorem ipsumloremipsumlorem ipsumlorem
+        ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumloremipsumlorem
+        ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
+        ipsumloremipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
+        ipsumlorem ipsumloremipsumlorem ipsumlorem ipsumlorem ipsumlorem
+        ipsumlorem ipsumlorem ipsumloremipsumlorem ipsumlorem ipsumlorem
+        ipsumlorem ipsumlorem ipsumlorem ipsumloremipsumlorem ipsumlorem
+        ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
+        ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
+        ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
+        ipsumlorem ipsum
+      </TextArea>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <Form></Form>
+
+           <br />
+      <br />
+      <br />
+
+      <Table
+        expense={SelectedItem}
+        deleteItem={(id) => setSelectedItem(SelectedItem.filter((item) => item.id !== id))}
+      ></Table>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+    </>
+    
+  );
 }
-
-
 
 export default App;
--- src/assets/body/ExpenceTracker/Table.tsx
+++ src/assets/body/ExpenceTracker/Table.tsx
@@ -1,56 +1,56 @@
-
-
-interface Expense{
-    id: number,
-    Description: string,
-    Amount: number,
-    category: string
+interface Expense {
+  id: number;
+  Description: string;
+  Amount: number;
+  category: string;
 }
 
-interface Props{
-    expense: Expense[]
+interface Props {
+  expense: Expense[];
+  deleteItem: (id: number) => void
 }
 
-export default function Table(props : Props){
-    
-    
-    
-    
-    return(
-        <div>
-            
-            <table className="table table-bordered w-50">
-                <thead>
-                <tr>
-                    <th scope="col">Description</th>
-                    <th scope="col">Amount</th>
-                    <th scope="col">Category</th>
-                    <th scope="col"></th>
-                </tr>
-                </thead>
-                <tbody>
-                
-                {props.expense.map((item) =>
-                    <tr>
-                    <td>{item.id}</td>
-                    <td>${item.Amount}</td>
-                    <td>{item.category}</td>
-                    <td>
-                        <button className={'btn btn-outline-danger'}>delete</button>
-                    </td>
-                </tr>)}
-                
-                </tbody>
-                
-                <tfoot>
-                    <tr>
-                    <td>total</td>
-                    <td colSpan={3}>${5.00}</td>
-                    </tr>
-                </tfoot>
-                
-            </table>
-            
-        </div>
-    )
+
+
+export default function Table(props: Props) {
+
+    if(props.expense.length === 0){
+        return null
+    }
+
+  return (
+    <div>
+      <table className="table table-bordered w-50">
+        <thead>
+          <tr>
+            <th scope="col">Description</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Category</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.expense.map((item) => (
+            <tr key={item.id}>
+              <td>{item.Description}</td>
+              <td>${item.Amount}</td>
+              <td>{item.category}</td>
+              <td>
+                <button className={"btn btn-outline-danger"} onClick={() => props.deleteItem(item.id)}>
+                  delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td>total</td>
+            <td colSpan={3}>${props.expense.reduce((acc, item) => acc + item.Amount, 0)}</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
 }
